a1747d3b96dd6403835cdf549ac4dad7ec73fa08
Haleigh Rzonca <hgielah@gmail.com>
2023-01-18 18:31:09 -0800

    added components

---
+++ portfolio/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact Me!</h1>
+            <li>Email: </li>
+            <li>Phone Number:</li>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <ul>
+            <li className='nav-item'>
+                <a href='https://github.com/Hrzonca'>
+                    GitHub
+                </a>
+            </li>
+            <li>
+                <a href='https://www.linkedin.com/in/haleigh-rzonca-794942249/'>
+                    LinkedIn
+                </a>
+            </li>
+        </ul>
+    )
+}
+
+export default Footer;
