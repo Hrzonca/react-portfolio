507a30755c238ef040fd33d5963d309c4eb13e30
Haleigh Rzonca <hgielah@gmail.com>
2023-01-18 18:51:15 -0800

    Working on the header file

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -3,9 +3,16 @@ import About from "./About";
 import Contact from "./Contact";
 import Portfolio from "./Portfolio"
 import Resume from "./Resume";
+import { NavLink } from 'react-router-dom'
 
-export default function Navigation() {
-    return {
-        
-    }
+function Navigation(props) {
+    return (
+        <div>
+            <NavLink path='/About' component={About}>About</NavLink>
+            <NavLink path="/Project" component={Portfolio}>Projects</NavLink>
+            <NavLink path="/Contact" component={Contact}>Contact</NavLink>
+            <NavLink path="/Resume" component={Resume}>Resume</NavLink>
+        </div>
+    )
 }
+export default Navigation
