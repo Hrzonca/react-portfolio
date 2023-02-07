92b73ce913d35f1ac385853b4641173019f9e961
Haleigh Rzonca <hgielah@gmail.com>
2023-02-07 14:35:41 -0800

    working on styling and using coolor colors

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Resume() {
     return (
-        <section class="mb-5">
+        <section className="mb-5">
         <h1 className="resume">Haleigh Rzonca</h1>
-        <div class="row justify-center"> 
+        <div className="row justify-center"> 
         <p>
            Highly motivated learner with a Bachelors in Biomedical sciences along with a certificate of completion for a full stack Javascript program, all through the University of Washington. I have spent 20 years in school and still can not get enough. I have found a passion for the compexity of computer science and want to explore it more. I decided to undertake a new field of study hoping my love for learning languages would translate over and it has paid off so far. I am excited to get into this field and learn as much as I can.
         </p>
